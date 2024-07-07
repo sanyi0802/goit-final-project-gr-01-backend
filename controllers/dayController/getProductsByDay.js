8e43f45e2994bc565f5208ce0cdf481105c7362e
Eduardo Jose Maya Rodriguez <ediij1994@gmail.com>
2024-07-06 20:47:17 -0500

    Tareas 7,8,9 realizadas

--- controllers/dayController/getProductsByDay.js
+++ controllers/dayController/getProductsByDay.js
@@ -1,7 +1,15 @@
+const { day } = require("../../schema/daySchema");
+
 const getProductsByDay = async (req, res) => {
   try {
-    // Logica ...
-  } catch (error) {}
+    const getProductOfTheDay = await day.find({
+      user: req.user,
+      date: req.body.date,
+    });
+    return res.status(200).json(getProductOfTheDay);
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 module.exports = getProductsByDay;
